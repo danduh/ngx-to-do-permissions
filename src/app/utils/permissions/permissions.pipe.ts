13c09c2cd0b2a821775a5fb3c0c5021c8c5f6dd2
Daniel Ostrovsky <daniel.ostrovsky@kaltura.com>
2020-11-29 22:25:50 +0200

    transfered to use observable

--- src/app/utils/permissions/permissions.pipe.ts
+++ src/app/utils/permissions/permissions.pipe.ts
@@ -1,23 +1,24 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {isPermitted, getPermissions} from './permissions.func';
+import {isPermitted} from './permissions.func';
 import {tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Pipe({
-    name: 'permissions'
+  name: 'permissions'
 })
 export class PermissionsPipe implements PipeTransform {
-    constructor() {
-    }
+  constructor() {
+  }
 
-    transform(required: any, args?: any): any {
+  transform(required: string, args?: any): Observable<boolean> {
 
-        return isPermitted(required)
-            .pipe(
-                tap((canDo) => {
-                    if (!canDo) {
-                        console.log('[PERMISSIONS PIPE] You don\'t have permissions');
-                    }
-                })
-            );
-    }
+    return isPermitted(required)
+      .pipe(
+        tap((canDo) => {
+          if (!canDo) {
+            console.log('[PERMISSIONS PIPE] You don\'t have permissions');
+          }
+        })
+      );
+  }
 }
