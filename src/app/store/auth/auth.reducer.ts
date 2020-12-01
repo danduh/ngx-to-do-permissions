3be3084f36c45bafd72458b42e9be644040d8fd9
Daniel Ostrovsky <daniel.ostrovsky@kaltura.com>
2020-12-01 18:10:46 +0200

    minor decor fix

--- src/app/store/auth/auth.reducer.ts
+++ src/app/store/auth/auth.reducer.ts
@@ -1,14 +1,14 @@
 const initialState = {
-    todos: {
-        read: '*',
-        update: '*',
-        create: '*',
-    },
-    stats: {
-        read: '*'
-    }
+  todos: {
+    read: '*',
+    update: '*',
+    create: '*',
+  },
+  stats: {
+    read: '*'
+  }
 };
 
 export function userPermissionsReducer(state = initialState, action) {
-    return {...state};
+  return {...state};
 }
