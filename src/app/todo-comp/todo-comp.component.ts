38f731cdb650a81ad050d36d6d456c3e26138f9a
Daniel Ostrovsky <daniel.ostrovsky@kaltura.com>
2020-12-01 15:46:18 +0200

    minor decor fix

--- src/app/todo-comp/todo-comp.component.ts
+++ src/app/todo-comp/todo-comp.component.ts
@@ -4,91 +4,92 @@ import {select, Store} from '@ngrx/store';
 import {AppState, metadataSelector, todosListSelector} from '../store';
 import * as FilterActions from '../store/filter/actions';
 import * as TodoActions from '../store/todos/todo.actions';
-
+import {Permissions} from '../utils/permissions/permissions.decorator';
 
 @Component({
-    selector: 'app-todo-comp',
-    templateUrl: './todo-comp.component.html',
-    styleUrls: ['./todo-comp.component.css']
+  selector: 'app-todo-comp',
+  templateUrl: './todo-comp.component.html',
+  styleUrls: ['./todo-comp.component.css']
 })
 
 export class TodoCompComponent implements OnInit {
-    todoList;
-    curFilter;
-    metadata;
+  todoList;
+  curFilter;
+  metadata;
 
-    constructor(private todoService: TodoService,
-                private store: Store<AppState>) {
+  constructor(private todoService: TodoService,
+              private store: Store<AppState>) {
 
-        this.store.dispatch(new TodoActions.GetTodos());
+    this.store.dispatch(new TodoActions.GetTodos());
 
-        this.curFilter = this.store
-            .pipe(
-                select('filter')
-            );
+    this.curFilter = this.store
+      .pipe(
+        select('filter')
+      );
 
-        this.todoList = this.store
-            .pipe(
-                select(todosListSelector),
-                // tap((_) => console.log(_))
-            );
-        this.store
-            .pipe(
-                select(metadataSelector),
-                // tap((_) => console.log(_))
-            ).subscribe(metadata => this.metadata = metadata);
+    this.todoList = this.store
+      .pipe(
+        select(todosListSelector),
+        // tap((_) => console.log(_))
+      );
+    this.store
+      .pipe(
+        select(metadataSelector),
+        // tap((_) => console.log(_))
+      ).subscribe(metadata => this.metadata = metadata);
 
 
+  }
+
+  // issues:
+  // 1. Because of saving in Store only todos that match current filter ...:
+  // 1.1 Counter always shows 0 if curFilter=="COMPLETED";
+  // 1.2 Clear completed button doesn't delete anything if curFilter=="ACTIVE";
+  // 1.3 Statistics component receives only the data which matches current filter though should receive ALL;
+  //
+  // 2. Need to store Error and Success flags in state to show content accordingly (error message or todolist);
+  // 3. Add nice css;
+  // 4. Add loading animation
+
+  updateCurFilter(filter) {
+    this.store.dispatch(new FilterActions.UpdateFilter(filter));
+  }
+
+  addTodo(newTodoVal: string): void {
+    const _todo = {
+      name: newTodoVal,
+      completed: false
+    };
+    this.store.dispatch(new TodoActions.AddTodo(_todo));
+  }
+
+  removeTodo(id: number): void {
+    console.log('from parent: ', id);
+    this.store.dispatch(new TodoActions.RemoveTodo(id));
+  }
+
+
+  editTodo(todo): void {
+    if (!!todo.name) {
+      this.store.dispatch(new TodoActions.UpdateTodo(todo));
+    } else {
+      this.removeTodo(todo.id);
     }
-
-    // issues:
-    // 1. Because of saving in Store only todos that match current filter ...:
-    // 1.1 Counter always shows 0 if curFilter=="COMPLETED";
-    // 1.2 Clear completed button doesn't delete anything if curFilter=="ACTIVE";
-    // 1.3 Statistics component receives only the data which matches current filter though should receive ALL;
-    //
-    // 2. Need to store Error and Success flags in state to show content accordingly (error message or todolist);
-    // 3. Add nice css;
-    // 4. Add loading animation
-
-    updateCurFilter(filter) {
-        this.store.dispatch(new FilterActions.UpdateFilter(filter));
-    }
-
-    addTodo(newTodoVal: string): void {
-        const _todo = {
-            name: newTodoVal,
-            completed: false
-        };
-        this.store.dispatch(new TodoActions.AddTodo(_todo));
-    }
-
-    removeTodo(id: number): void {
-        console.log('from parent: ', id);
-        this.store.dispatch(new TodoActions.RemoveTodo(id));
-    }
+  }
 
 
-    editTodo(todo): void {
-        if (!!todo.name) {
-            this.store.dispatch(new TodoActions.UpdateTodo(todo));
-        } else {
-            this.removeTodo(todo.id);
-        }
-    }
+  completeToggle(todo): void {
+    todo.completed = !todo.completed;
+    this.store.dispatch(new TodoActions.UpdateTodo(todo));
+  }
 
+  @Permissions('todos_delete')
+  clearCompleted(): void {
+    this.store.dispatch(new TodoActions.RemoveCompletedTodos());
+  }
 
-    completeToggle(todo): void {
-        todo.completed = !todo.completed;
-        this.store.dispatch(new TodoActions.UpdateTodo(todo));
-    }
+  ngOnInit() {
 
-    clearCompleted(): void {
-        this.store.dispatch(new TodoActions.RemoveCompletedTodos());
-    }
-
-    ngOnInit() {
-
-    }
+  }
 
 }
